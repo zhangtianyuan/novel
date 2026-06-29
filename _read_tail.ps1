95b9317206bbd11b33daf56a2286797b2667fe14
zhangtianyuan <zty169@ustc.edu>
2026-06-29 13:02:37 +0800

    第13章 地下有物

--- _read_tail.ps1
+++
@@ -1,5 +0,0 @@
-$content = Get-Content 'C:\Users\Admin\.openclaw\workspace\novel\正文.md' -Encoding UTF8
-$total = $content.Count
-$start = [Math]::Max(0, $total - 400)
-$lines = $content[$start..($total-1)]
-Write-Output ($lines -join "`r`n")
